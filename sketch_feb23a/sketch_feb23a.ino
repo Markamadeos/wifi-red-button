2458b85732b6b996f00f47611ec8750dc13b87cb
Mark Quarasique <julipaha@gmail.com>
2024-02-25 21:07:27 +0300

    Мигрируем на littleFS

--- sketch_feb23a/sketch_feb23a.ino
+++ sketch_feb23a/sketch_feb23a.ino
@@ -1,183 +1,164 @@
-#include <FS.h>           //this needs to be first, or it all crashes and burns...
+#include <FS.h>
+#include <LittleFS.h>
 #include <WiFiManager.h>  //https://github.com/tzapu/WiFiManager
-
-#ifdef ESP32
-#include <SPIFFS.h>
-#endif
-
-#include <ArduinoJson.h>  //https://github.com/bblanchon/ArduinoJson
 #include <HTTPClient.h>
 #include <WiFi.h>
 
-//define your default values here, if there are different values in config.json, they are overwritten.
-char login[10] = "admin";
-char pass[16] = "";
-char gateway[20] = "";
-char command[40] = "";
-const int buttonPin = 35;
-const int ledPin = 15;
+#define BUTTON_PIN 35
+#define LED_PIN 15
+#define FORMAT_FS_NEEDED true
+#define AP_SSID "wifi-red-button"
+#define AP_PASSWORD "P@ssw0rd"
+#define HTTP_PREFIX "http://"
+#define HTTP_POSTFIX "/rest/system/"
+#define GATEWAY_WM_PARAM_ID "gateway"
+#define LOGIN_WM_PARAM_ID "login"
+#define PASSWORD_WM_PARAM_ID "pass"
+#define COMMAND_WM_PARAM_ID "command"
+#define COMMAND_OPTION_WM_PARAM_ID "commandOption"
+#define CONFIG_FILE_PATH "/config.txt"
+
+typedef struct {
+  char routerLogin[20] = "";
+  char routerPass[20] = "";
+  char routerGateway[20] = "";
+  char apiCommand[20] = "";
+  char apiCommandOption[20] = "";
+} parameters;
+
+parameters params;
 int lastState = LOW;
 int currentState;
-String httpPrefix = "http://";
-String httpPostfix = "/rest/system/";
-char ssid[40] = "wifi-red-button";
-char password[40] = "P@ssw0rd";
-
-//flag for saving data
 bool shouldSaveConfig = false;
 
-//WiFiManager
-WiFiManager wifiManager;
-WiFiManagerParameter gatewayIp("gateway", "gateway IP", gateway, 20);
-WiFiManagerParameter apiLogin("login", "api login", login, 10);
-WiFiManagerParameter apiPass("pass", "api password", pass, 16);
-WiFiManagerParameter apiCommand("command", "command (http://{gateway}/rest/system/{command})", command, 40);
+WiFiManagerParameter gatewayIp(GATEWAY_WM_PARAM_ID, "gateway IP", params.routerGateway, 20);
+WiFiManagerParameter apiLogin(LOGIN_WM_PARAM_ID, "api login", params.routerLogin, 20);
+WiFiManagerParameter apiPass(PASSWORD_WM_PARAM_ID, "api password", params.routerPass, 20);
+WiFiManagerParameter apiCommand(COMMAND_WM_PARAM_ID, "command (http://{gateway}/rest/system/{command})", params.apiCommand, 20);
+WiFiManagerParameter apiCommandOption(COMMAND_OPTION_WM_PARAM_ID, "ex. force = yes", params.apiCommandOption, 20);
 
-//callback notifying us of the need to save config
 void saveConfigCallback() {
   Serial.println("Should save config");
   shouldSaveConfig = true;
 }
 
-void resetEspSettings() {
-  digitalWrite(ledPin, HIGH);
-  delay(2000);
-  digitalWrite(ledPin, LOW);
-  SPIFFS.format();
-  wifiManager.resetSettings();
-  ESP.restart();
-}
+void saveConfig(fs::FS &fs) {
+  Serial.printf("Writing file: %s\r\n", CONFIG_FILE_PATH);
 
-void readConfigFromJson() {
-  //read configuration from FS json
-  Serial.println("mounting FS...");
-
-  if (SPIFFS.begin()) {
-    Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
-      Serial.println("reading config file");
-      File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile) {
-        Serial.println("opened config file");
-        size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
-        std::unique_ptr<char[]> buf(new char[size]);
-        configFile.readBytes(buf.get(), size);
-#if defined(ARDUINOJSON_VERSION_MAJOR) && ARDUINOJSON_VERSION_MAJOR >= 6
-        DynamicJsonDocument json(1024);
-        auto deserializeError = deserializeJson(json, buf.get());
-        serializeJson(json, Serial);
-        if (!deserializeError) {
-#else
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& json = jsonBuffer.parseObject(buf.get());
-        json.printTo(Serial);
-        if (json.success()) {
-#endif
-          Serial.println("\nparsed json");
-          strcpy(gateway, json["gateway"]);
-          strcpy(login, json["login"]);
-          strcpy(pass, json["pass"]);
-          strcpy(command, json["command"]);
-        } else {
-          Serial.println("failed to load json config");
-        }
-        configFile.close();
-      }
-    }
+  File file = fs.open(CONFIG_FILE_PATH, FILE_WRITE);
+  if (!file) {
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if (file.println(params.routerLogin)
+      || file.println(params.routerPass)
+      || file.println(params.routerGateway)
+      || file.println(params.apiCommand)
+      || file.println(params.apiCommandOption)) {
+    Serial.println("- file written");
   } else {
-    Serial.println("failed to mount FS");
+    Serial.println("- write failed");
+  }
+  file.close();
+}
+
+void readConfig(fs::FS &fs) {
+  Serial.printf("Reading file: %s\r\n", CONFIG_FILE_PATH);
+
+  File file = fs.open(CONFIG_FILE_PATH);
+  if (!file || file.isDirectory()) {
+    Serial.println("- failed to open file for reading");
+    return;
+  }
+
+  Serial.println("- read from file:");
+  while (file.available()) {
+    Serial.println(file.readStringUntil('\n'));
+  }
+  file.close();
+}
+
+void resetEspSettings() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(2000);
+  digitalWrite(LED_PIN, LOW);
+  if (!LittleFS.begin(FORMAT_FS_NEEDED)) {
+    Serial.println("LittleFS Mount Failed");
   }
 }
 
-void saveConfigToJson() {
-  Serial.println("saving config");
-#if defined(ARDUINOJSON_VERSION_MAJOR) && ARDUINOJSON_VERSION_MAJOR >= 6
-  DynamicJsonDocument json(1024);
-#else
-  DynamicJsonBuffer jsonBuffer;
-  JsonObject& json = jsonBuffer.createObject();
-#endif
-  json["login"] = login;
-  json["pass"] = pass;
-  json["gateway"] = gateway;
-  json["command"] = command;
-
-  File configFile = SPIFFS.open("/config.json", "w");
-  if (!configFile) {
-    Serial.println("failed to open config file for writing");
-  }
-
-#if defined(ARDUINOJSON_VERSION_MAJOR) && ARDUINOJSON_VERSION_MAJOR >= 6
-  serializeJson(json, Serial);
-  serializeJson(json, configFile);
-#else
-  json.printTo(Serial);
-  json.printTo(configFile);
-#endif
-  configFile.close();
+void sendCommand() {
+  WiFiClient client;
+  HTTPClient http;
+  // setup URL
+  String url = HTTP_PREFIX + String(params.routerGateway) + HTTP_POSTFIX + String(params.apiCommand);
+  Serial.println(url);
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, url);
+  // If you need Node-RED/server authentication, insert user and password below
+  http.setAuthorization(params.routerLogin, params.routerPass);
+  http.setAuthorizationType("Basic");
+  // If you need an HTTP request with a content type: application/json, use the following:
+  http.addHeader("Content-Type", "application/json");
+  int httpResponseCode = http.POST("{}");
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+  Serial.println(url);
+  // Free resources
+  http.end();
 }
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(ledPin, OUTPUT);
-  currentState = digitalRead(buttonPin);
-  readConfigFromJson();
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-  wifiManager.addParameter(&gatewayIp);
-  wifiManager.addParameter(&apiLogin);
-  wifiManager.addParameter(&apiPass);
-  wifiManager.addParameter(&apiCommand);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  readConfig(LittleFS);
+  WiFiManager wm;
+  currentState = digitalRead(BUTTON_PIN);
   if (currentState == HIGH) {
     resetEspSettings();
-  }
-  if (!wifiManager.autoConnect(ssid, password)) {
-    Serial.println("failed to connect and hit timeout");
-    delay(3000);
-    //reset and try again, or maybe put it to deep sleep
+    wm.resetSettings();
     ESP.restart();
     delay(5000);
   }
-  //if you get here you have connected to the WiFi
-  Serial.println("connected...yeey :)");
+  Serial.begin(115200);
+  while (!Serial) {
+    ;  // wait for serial port to connect
+  }
 
-  strcpy(login, apiLogin.getValue());
-  strcpy(pass, apiPass.getValue());
-  strcpy(gateway, gatewayIp.getValue());
-  strcpy(command, apiCommand.getValue());
-  saveConfigToJson();
+  // WiFi.mode(WIFI_STA);
+  //wm.setSaveConfigCallback(saveConfigCallback);
+  wm.addParameter(&gatewayIp);
+  wm.addParameter(&apiLogin);
+  wm.addParameter(&apiPass);
+  wm.addParameter(&apiCommand);
+  wm.addParameter(&apiCommandOption);
+  wm.setSaveConfigCallback(saveConfigCallback);
+
+  if (!wm.autoConnect(AP_SSID, AP_PASSWORD)) {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    ESP.restart();
+    delay(5000);
+  }
+
+  Serial.println("connected...yeey :)");
+  strcpy(params.routerLogin, apiLogin.getValue());
+  strcpy(params.routerPass, apiPass.getValue());
+  strcpy(params.routerGateway, gatewayIp.getValue());
+  strcpy(params.apiCommand, apiCommand.getValue());
+  strcpy(params.apiCommandOption, apiCommandOption.getValue());
+  Serial.println(params.apiCommand);
+  saveConfig(LittleFS);
   Serial.println("local ip");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  currentState = digitalRead(buttonPin);
+  currentState = digitalRead(BUTTON_PIN);
   if (currentState == HIGH) {
-    //Check WiFi connection status
     if (WiFi.status() == WL_CONNECTED) {
-      WiFiClient client;
-      HTTPClient http;
-      // setup URL
-      String url = httpPrefix + gateway + httpPostfix + command;
-      Serial.println(url);
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, url);
-      // If you need Node-RED/server authentication, insert user and password below
-      http.setAuthorization(login, pass);
-      http.setAuthorizationType("Basic");
-      // If you need an HTTP request with a content type: application/json, use the following:
-      http.addHeader("Content-Type", "application/json");
-      int httpResponseCode = http.POST("{}");
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-      Serial.println(url);
-      // Free resources
-      http.end();
-    } else {
-      Serial.println("WiFi Disconnected");
+      sendCommand();
+      delay(3000);
     }
   }
 }
